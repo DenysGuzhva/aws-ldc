6e155e0a0a3d853973abb2e487293cd6ef845731
Denys Pozyvailov <cerellador@protonmail.com>
2026-03-16 14:07:57 +0100

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
+FROM gradle:8-jdk17 AS build
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN ./gradlew bootJar --no-daemon
+
 FROM amazoncorretto:17
-COPY build/libs/*.jar app.jar
-ENTRYPOINT ["java", "-jar", "/app.jar"]
+EXPOSE 8080
+COPY --from=build /home/gradle/src/build/libs/*.jar app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/dp/awsldc/HelloAWS.java
+++ src/main/java/com/dp/awsldc/HelloAWS.java
@@ -10,10 +10,18 @@ public class HelloAWS {
 
   @GetMapping("/")
   public String getHostIp() {
+    // Look for the Environment Variable first
+    String serverName = System.getenv("SERVER_NAME");
+
+    if (serverName != null && !serverName.isEmpty()) {
+      return "Connected to: " + serverName;
+    }
+
+    // Fallback to the IP address if the variable isn't set
     try {
       return "Server IP Address: " + InetAddress.getLocalHost().getHostAddress();
     } catch (UnknownHostException e) {
-      return "Error retrieving IP: " + e.getMessage();
+      return "Error: " + e.getMessage();
     }
   }
 
